6278447dde5200d92efc30e362127c861fae0626
=Mahsa <mahsa.gol89@gmail.com>
2021-03-24 03:10:06 +0430

    core form uddate clean

--- app/core/forms.py
+++ app/core/forms.py
@@ -1,18 +1,55 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.forms import ValidationError as FormValidationError
 
-from .models import Teacher
+from .models import Subject, Teacher
 
 # A teacher can teach no more than 5 subjects
+
+class SubjectInlineForm(forms.ModelForm):
+    
+    subjects = forms.ModelChoiceField(queryset=Subject.objects.all())
+    
+    class Meta:
+        model = Teacher.subjects.through
+        fields = '__all__'
+    
+    
+    def __init__(self, *args, **kwargs):
+        super(SubjectInlineForm, self).__init__(*args, **kwargs)
+        if kwargs.get('instance'):
+            teacher = Teacher.objects.get(id=self.instance.id)
+            self.fields['subjects'].initial = teacher.subjects
+            
+    def save(self, commit=True):
+        instance = super(SubjectInlineForm, self).save(commit=False)
+        teacher = Teacher.objects.get(id=instance.id)
+        teacher.subjects = self.cleaned_data['subjects']
+        teacher.save()
+
+        if commit:
+            instance.save()
+        return instance
+    
+    def clean(self):
+        super(SubjectInlineForm, self).clean(*args, **kwargs)
+        teacher = Teacher.objects.get(id=instance.id)
+        teacher.subjects = self.cleaned_data['subjects']
+        if teacher.subjects.__len__() > 5: 
+            raise FormValidationError("Can't add more than 5 subjects to a Teacher")
+        return self.cleaned_data
+    
 class TeacherForm(forms.ModelForm):
     """ 
-    A teacher can teach no more than 5 subjects
-    """
+    A teacher can teach no more than 5 subjects 
+    """    
     class Meta:
         model = Teacher
-        exclude = [id, 'date_removed', 'validation_error',]
+        exclude = [id, 'date_removed',]
 
-    def clean(self):
+
+    def clean_subjects(self):
         subjects = self.cleaned_data.get('subjects')
         if subjects.__len__() > 5: 
-            raise forms.ValidationError("Can't add more than 5 subjects to a Teacher")
-        return self.cleaned_data
+            raise FormValidationError("Can't add more than 5 subjects to a Teacher")
+        return subjects
